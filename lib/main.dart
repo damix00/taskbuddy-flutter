d83f8f2138c8710a7bc7159bc40fea45a67fe1bd
Damjan Pavličević <damix4921@gmail.com>
2023-08-14 02:24:11 +0200

    i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:taskbuddy/util/constants.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,14 +11,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       title: 'Flutter Demo',
       themeMode: ThemeMode.system,
       darkTheme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
         colorScheme: const ColorScheme(
           background: Constants.bgColor,
           brightness: Brightness.dark,
@@ -32,6 +38,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
         colorScheme: const ColorScheme(
           background: Colors.white,
           brightness: Brightness.dark,
@@ -62,9 +69,9 @@ class App extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text('latinary fanboy', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          'Hello, world!',
+          AppLocalizations.of(context)!.helloWorld,
         ),
       ),
     );
