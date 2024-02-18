651cd8d10b57c5879de9f26c20f7acedfa8b3de2
Damjan Pavličević <damix4921@gmail.com>
2024-02-18 18:18:55 +0100

    remove needless variable

--- lib/api/options.dart
+++ lib/api/options.dart
@@ -6,7 +6,6 @@ class ApiOptions {
   static String get fullDomain => 'https://$domain';
   static String get baseUrl => kReleaseMode ? 'https://$domain' : 'http://192.168.1.18:9500';
   // static String get baseUrl => 'http://192.168.1.18:9500';
-  static String get socketUrl => 'http://192.168.1.18:9500/socket.io/?EIO=4&transport=polling';
   static String get version => '/v1';
   static String get path => '$baseUrl$version';
   static String userAgent = '${Platform.operatingSystem}/${Platform.operatingSystemVersion}';
