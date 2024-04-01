70f905b9b53c51fb4fae67e7af4127f25b94a65e
Damjan Pavličević <damix4921@gmail.com>
2024-04-01 18:34:12 +0200

    update app version, ready for release

--- lib/api/options.dart
+++ lib/api/options.dart
@@ -8,6 +8,6 @@ class ApiOptions {
   // static String get baseUrl => 'http://192.168.1.18:9500';
   static String get version => '/v1';
   static String get path => '$baseUrl$version';
-  static String get appVersion => '1.0a';
+  static String get appVersion => '1.2.1';
   static String userAgent = '${Platform.operatingSystem}/${Platform.operatingSystemVersion}/$appVersion';
 }
